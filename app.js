7bf2ebb8585b8e5e47bf938dd46b8315b0d6b9c9
Jawadbutt861 <jawadbutt861@gmail.com>
2025-10-12 18:22:55 +0500

    updated

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 let apiEverthing = "https://newsapi.org/v2/everything?q=football&apiKey=9aeadfb143464e0ead47b26f81743bf0";
 let apiTrending = "https://newsapi.org/v2/top-headlines?country=us&apiKey=9aeadfb143464e0ead47b26f81743bf0";
+let arr = []
 
 fetch(apiTrending)
 .then((res)=>{
     return res.json();
 })
 .then((res)=>{
-    console.log(res);
+    for(let i = 0; i < res.articles.length; i++){
+        let obj = {
+        title : `${res.articles[i].title}`,
+        description : `${res.articles[i].description}`,
+        content : `${res.articles[i].content}`,
+        channelName : `${res.articles[i].source.name}`,
+        author : `${res.articles[i].author}`,
+        time : `${res.articles[i].publishedAt}`,
+        detail : `${res.articles[i].url}`,
+    }
+    arr.push(obj)
+    }
+        
+        
+    
+   
+    console.log(arr);
     
     return res;
 })
